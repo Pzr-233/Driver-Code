f254c103a3f36cbcaf4dd03f3e284fb40946d068
Pzr <3375240353@qq.com>
2025-11-23 14:59:23 +0800

    完善了按键逻辑与可移植性

--- General_Driver/Key/Get_Key.c
+++ General_Driver/Key/Get_Key.c
@@ -1,8 +1,7 @@
-#include "gpio.h"
-#include "tim.h"
 #include "Get_Key.h"
 
-KeyManager_s KeyManager = {// 初始化按键管理结构体
+KeyManager_s KeyManager = {
+    // 初始化按键管理结构体,注册按键
     .Key = {
         {GPIOC, GPIO_PIN_1, Key_1, Key_Released, Key_None, 0},
         {GPIOC, GPIO_PIN_2, Key_2, Key_Released, Key_None, 0},
@@ -22,9 +21,9 @@ uint32_t getKeyScanFreq(void)
 
 void Key_Init(void)
 {
-    setKeyScanFreq(50);// 默认按键扫描频率50Hz
-    KeyManager.keyDuobleClickTime = getKeyScanFreq() / 10;// 双击时间间隔为100ms
-    KeyManager.keyLongPressTime   = getKeyScanFreq() / 2;// 长按时间为500ms
+    setKeyScanFreq(50);                                    // 默认按键扫描频率50Hz
+    KeyManager.keyDuobleClickTime = getKeyScanFreq() / 10; // 双击时间间隔为100ms
+    KeyManager.keyLongPressTime   = getKeyScanFreq() / 2;  // 长按时间为500ms
 }
 
 void Key_Scan(void)
@@ -32,34 +31,34 @@ void Key_Scan(void)
     uint8_t pinState = 0;
     for (uint8_t i = 0; i < Key_Number; i++) {                                            // 遍历所有按键
         pinState = HAL_GPIO_ReadPin(KeyManager.Key[i].GPIOx, KeyManager.Key[i].GPIO_Pin); // 读取按键状态
-        if (pinState == GPIO_PIN_RESET)                                                   // 按键按下
+        if (pinState == PRESSED)                                                          // 按键按下
         {
-            if (KeyManager.Key[i].Time_Counter <= KeyManager.keyLongPressTime) // 未达到长按时间
+            if (KeyManager.Key[i].Time_Counter <= KeyManager.keyLongPressTime || KeyManager.Key[i].doubleClickFlag == 1) // 未达到长按时间或正在检测双击
             {
                 KeyManager.Key[i].State = Key_Pressed;
                 KeyManager.Key[i].Time_Counter++;
-                if (KeyManager.Key[i].duobleClick_Counter != 0) { // 已有一次按下，准备进行双击检测
-                    KeyManager.Key[i].doubleClickFlag     = 1;    // 标记为双击
-                    KeyManager.Key[i].duobleClick_Counter = 0;    // 清除双击计数器
+                if (KeyManager.Key[i].duobleClick_Counter != 0) {         // 已有一次按下，准备进行双击检测
+                    KeyManager.Key[i].Action          = Key_Double_Click; // 双击动作
+                    KeyManager.Key[i].doubleClickFlag = 1;                // 标记为双击
                 }
             } else // 达到长按时间
             {
                 KeyManager.Key[i].Action        = Key_Long_Pres; // 长按动作
                 KeyManager.Key[i].longPressFlag = 1;             // 标记为长按
             }
-        } else if (pinState == GPIO_PIN_SET && KeyManager.Key[i].State == Key_Pressed && KeyManager.Key[i].doubleClickFlag != 1 && KeyManager.Key[i].longPressFlag != 1) // 按键释放且未标记为双击或长按
+        } else if (pinState == RELEASED && KeyManager.Key[i].State == Key_Pressed && KeyManager.Key[i].doubleClickFlag != 1 && KeyManager.Key[i].longPressFlag != 1) // 按键释放且未标记为双击或长按,延时检测双击
         {
-            KeyManager.Key[i].duobleClick_Counter++;
-            KeyManager.Key[i].Time_Counter = 0;
+            KeyManager.Key[i].duobleClick_Counter++; // 增加双击计数器
+            KeyManager.Key[i].Time_Counter = 0;      // 清除时间计数器
             if (KeyManager.Key[i].duobleClick_Counter > KeyManager.keyDuobleClickTime) {
                 KeyManager.Key[i].Action              = Key_Click;    // 单击动作
                 KeyManager.Key[i].State               = Key_Released; // 更新状态为释放
                 KeyManager.Key[i].duobleClick_Counter = 0;            // 清除双击计数器
             }
-        } else if (pinState == GPIO_PIN_SET) {
-            if (KeyManager.Key[i].doubleClickFlag == 1) {
-                KeyManager.Key[i].Action          = Key_Double_Click; // 双击动作
-                KeyManager.Key[i].doubleClickFlag = 0;                // 清除双击标志
+        } else if (pinState == RELEASED) {                 // 按键释放
+            if (KeyManager.Key[i].doubleClickFlag == 1) {  // 清除双击标志与动作
+                KeyManager.Key[i].doubleClickFlag     = 0; // 清除双击标志
+                KeyManager.Key[i].duobleClick_Counter = 0; // 清除双击计数器
             }
             if (KeyManager.Key[i].longPressFlag == 1) { // 清除长按标志与动作
                 KeyManager.Key[i].longPressFlag = 0;
@@ -76,7 +75,7 @@ Key_Action_e Read_KeyState(Key_Number_e keyNum)
     Key_Action_e action = 0;
     switch (KeyManager.Key[keyNum].Action) {
         case Key_Long_Pres:
-            action = Key_Long_Pres;         // 读取后不清除动作，长按需要持续读取
+            action = Key_Long_Pres; // 读取后不清除动作，长按需要持续读取
             break;
         case Key_Double_Click:
             action                        = Key_Double_Click;
--- General_Driver/Key/Get_Key.h
+++ General_Driver/Key/Get_Key.h
@@ -1,34 +1,31 @@
 #ifndef __GET_KEY_H
 #define __GET_KEY_H
 #include "gpio.h"
-#include "tim.h"
-#define Key_tim &htim11
+#define PRESSED  GPIO_PIN_RESET // 按下时GPIO状态
+#define RELEASED GPIO_PIN_SET   // 释放时GPIO状态
 
-typedef enum
-{
+typedef enum {
     Key_Pressed,
     Key_Released,
-}Key_State_e;
+} Key_State_e;
 
-typedef enum
-{
-    Key_None=0,
+typedef enum {
+    Key_None = 0,
     Key_Click,
     Key_Double_Click,
     Key_Long_Pres,
-}Key_Action_e;
+} Key_Action_e; // 按键动作枚举
 
-typedef enum
-{
+typedef enum {
     Key_1,
     Key_2,
     Key_3,
     Key_Number,
-}Key_Number_e;
+} Key_Number_e; // 按键数量
 
-typedef struct 
+typedef struct
 {
-    GPIO_TypeDef* GPIOx;
+    GPIO_TypeDef *GPIOx;
     uint16_t GPIO_Pin;
     Key_Number_e Number;
     Key_State_e State;
@@ -37,15 +34,15 @@ typedef struct
     uint32_t duobleClick_Counter;
     uint8_t doubleClickFlag;
     uint8_t longPressFlag;
-}Key_s;
+} Key_s; // 按键结构体
 
-typedef struct 
+typedef struct
 {
-    uint32_t Key_Scan_Freq;         //按键扫描频率，单位Hz
-    uint32_t keyLongPressTime;      //长按时间，单位为扫描次数
-    uint32_t keyDuobleClickTime;    //双击时间间隔，单位为扫描次数
+    uint32_t Key_Scan_Freq;      // 按键扫描频率，单位Hz
+    uint32_t keyLongPressTime;   // 长按时间，单位为扫描次数
+    uint32_t keyDuobleClickTime; // 双击时间间隔，单位为扫描次数
     Key_s Key[Key_Number];
-}KeyManager_s;
+} KeyManager_s; // 按键管理结构体
 
 void Key_Init(void);
 void Key_Scan(void);
@@ -53,10 +50,4 @@ Key_Action_e Read_KeyState(Key_Number_e keyNum);
 uint32_t getKeyScanFreq(void);
 void setKeyScanFreq(uint32_t freq); // 设置按键扫描频率，单位Hz
 
- 
-
-
-
-
 #endif
-
